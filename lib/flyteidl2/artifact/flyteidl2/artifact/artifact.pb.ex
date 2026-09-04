95af6b322b7622a6bab86893b4bdca62882a1cd6
mruoss <695307+mruoss@users.noreply.github.com>
2026-09-04 09:15:02 +0000

    [create-pull-request] automated change

--- lib/flyteidl2/artifact/flyteidl2/artifact/artifact.pb.ex
+++ lib/flyteidl2/artifact/flyteidl2/artifact/artifact.pb.ex
@@ -66,7 +66,12 @@ defmodule Flyteidl2.Artifact.ArtifactSpec do
   field :type, 2, type: Flyteidl2.Core.LiteralType, deprecated: false
   field :info, 3, type: Flyteidl2.Core.ArtifactInfo
   field :source, 6, type: Flyteidl2.Artifact.ArtifactSource
-  field :parent_artifact, 7, type: Flyteidl2.Core.ArtifactVersionId, json_name: "parentArtifact"
+
+  field :parent_artifacts, 7,
+    repeated: true,
+    type: Flyteidl2.Core.ArtifactVersionId,
+    json_name: "parentArtifacts",
+    deprecated: false
 end
 
 defmodule Flyteidl2.Artifact.Artifact do
